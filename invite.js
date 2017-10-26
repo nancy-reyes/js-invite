99aabfd575745a95d49932b045faec89a2a1ad0b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-26 15:49:41 -0700

    Started Part 2

---
+++ invite.js
@@ -0,0 +1,78 @@
+"use strict";
+
+class Invitation {
+    // Right now 'displayElement' is equal to <div id="invite-display"></div>
+    constructor(displayElement){
+        // Below the class is adding HTML string to the object upon creation
+        displayElement.append(`<img id="image">
+                              <h1 id="title"></h1>
+                              <p id="date"></p>
+                              <p id="start"></p>
+                              <p id="end"></p>
+                              <p id="location"> </p>
+                              <p id="description"></p>`);
+
+        // Set properties on the class
+        this.container = displayElement;
+        this.imageEl = displayElement.children("img#image");
+        this.titleEl = displayElement.children("h1#title");
+        this.dateEl = displayElement.children("p#date");
+        this.startEl = displayElement.children("p#start");
+        this.endEl = displayElement.children("p#end");
+        this.locationEl = displayElement.children("p#location");
+        this.descriptionEl = displayElement.children("p#description");
+
+    }
+
+    changeTitle(newTitle){
+        this.titleEl.text(newTitle);
+
+    }
+
+    changeDate(newDate) {
+        this.dateEl.text("Join us on " + newDate);
+    }
+
+    changeStart(newStart) {
+        this.startEl.text(newStart);
+    }
+
+    changeEnd(newEnd) {
+        this.endEl.text(newEnd);
+    }
+
+    changeImage(newImage){
+        this.imageEl.attr("src", newImage);
+    }
+
+    changeFont(newFont) {
+        this.container.css("font-family", newFont);
+    }
+
+    // Add more methods here
+    changeLocation(newLocation) {
+        this.locationEl.text(newLocation);
+    }
+
+    changeDescription(newDescription) {
+        this.descriptionEl.text(newDescription);
+    }
+
+    changeBackgroundColor(newBackgroundColor) {
+        this.container.css("background-color", newBackgroundColor);
+    }
+
+}
+
+
+// PART 2:
+
+// Create your invitation object
+let invitationElement = $("#invite-display");
+let invitation = new Invitation(invitationElement);
+
+// Add event listeners
+// using JQuery to get what the user has entered in the form.
+$("#title-input").on('keypress', function(evt) {
+    invitation.changeTitle($("#title-input").val());
+});
